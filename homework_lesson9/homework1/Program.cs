86c1d520a0ade7d9c50f602dbb384cfa01acd944
Bulat <b.i.akhmedov@yandex.ru>
2023-04-23 10:23:07 +0300

    end homework lesson9

---
+++ homework_lesson9/homework1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+Console.WriteLine("Введите N:");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isParsedN) { Console.WriteLine("Ошибка ввода, введено не число"); return; }
+
+Print(n);
+
+void Print(int n, int start = 1)
+{
+    if (start > n) { return; }
+
+    Console.WriteLine(start);
+    start++;
+    
+    Print(n, start);
+
+}
---
+++ homework_lesson9/homework2/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.WriteLine("Введите начало диапазона:");
+bool isParsedStart = int.TryParse(Console.ReadLine(), out int start);
+
+Console.WriteLine("Введите конец диапазона:");
+bool isParsedStop = int.TryParse(Console.ReadLine(), out int stop);
+
+if (!isParsedStart || !isParsedStop) { Console.WriteLine("Ошибка ввода, введено не число"); return;}
+
+Print(stop);
+
+void Print(int n)
+{
+    if (start > stop) { return; }
+
+    Console.WriteLine(start);
+    start++;
+    
+    Print(n);
+
+}
